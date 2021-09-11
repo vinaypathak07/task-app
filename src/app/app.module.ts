c066b6b6aa8e43915fe4a2241ee50d4681589261
vinaypathak <nbp.vinay@gmail.com>
2021-09-11 11:34:11 +0530

    Added Drag And Drop Functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyTaskComponent } from './my-task/my-task.component';
 import { TaskStatusComponent } from './my-task/task-status/task-status.component';
+import {MatRippleModule} from '@angular/material/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { TaskStatusComponent } from './my-task/task-status/task-status.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatRippleModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
