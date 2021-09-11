d0c29a65d3cb1a30ca2dda38ebc5a777dfa068f7
vinaypathak <nbp.vinay@gmail.com>
2021-09-11 10:23:57 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyTaskComponent } from './my-task/my-task.component';
+import { TaskStatusComponent } from './my-task/task-status/task-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTaskComponent,
+    TaskStatusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
